f528127140d1dbaa827c6570e0435a030ca8a4d5
Sam Adams <119465691+SamAdams1@users.noreply.github.com>
2025-03-09 11:01:30 -0400

    header bar prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,51 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
-const localKey = "ewasteCount"
+const localKey = "ewasteCount";
 function getData() {
-  let localCount = localStorage.getItem(localKey)
+  let localCount = localStorage.getItem(localKey);
 
-    if (localCount) {
-      console.log(localCount)
-      return parseInt(localCount)
-    } else {
-      localStorage.setItem(localKey, "0")
-      return 0
-    }
+  if (localCount) {
+    console.log(localCount);
+    return parseInt(localCount);
+  } else {
+    localStorage.setItem(localKey, "0");
+    return 0;
+  }
 }
 
 function App() {
-  const [count, setCount] = useState(getData)
-  
-  
-  useEffect(() => {
-    
-  }, []) // fetch database data at start of program
+  const [count, setCount] = useState(getData);
+  const [currentPage, setCurrentPage] = useState("map");
 
   useEffect(() => {
-    localStorage.setItem(localKey, count.toString())
-  }, [count])
-  
+    localStorage.setItem(localKey, count.toString());
+  }, [count]);
 
   return (
     <>
-      <h1>SNHU E-Waste</h1>
+      <div className="header">
+        <img
+          src="/src/assets/snhuLogo.webp"
+          alt="snhuLogo"
+          className="snhuLogo"
+        />
+        <h1>SNHU E-Waste</h1>
+        <div className="headerButtons">
+          <button>Map</button>
+          <button>Database</button>
+        </div>
+      </div>
       <div className="card">
         <h2>Number of laptops: {count}</h2>
-        <button onClick={() => setCount((count) => count + 1)}>
-          +
-        </button>
-        <button onClick={() => setCount((count) => count - 1)}>
-          -
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>+</button>
+        <button onClick={() => setCount((count) => count - 1)}>-</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
