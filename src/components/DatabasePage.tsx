de1d7d77d0c3cf251a22a6799bd8e0dec83914c9
Sam Adams <119465691+SamAdams1@users.noreply.github.com>
2025-03-09 11:26:32 -0400

    images prototype

--- src/components/DatabasePage.tsx
+++ src/components/DatabasePage.tsx
@@ -3,7 +3,6 @@ import React from "react";
 const DatabasePage = () => {
   return (
     <div>
-      DatabasePage
       <img src="src\assets\tablePlaceholder.png" alt="" />
     </div>
   );
--- src/components/MapPage.tsx
+++ src/components/MapPage.tsx
@@ -3,8 +3,7 @@ import React from "react";
 const MapPage = () => {
   return (
     <div>
-      MapPage
-      <img src="src\assets\SNHU-Campus-Map-2024.webp" alt="" />
+      <img src="src\assets\snhuMap.png" alt="" />
     </div>
   );
 };
