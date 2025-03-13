3491aac22cbc9eb40562d61a277a6b562e0f6165
Sam Adams <sammyadams04@gmail.com>
2025-03-13 15:01:27 -0400

    fake data table front end

--- src/components/DatabasePage.tsx
+++ src/components/DatabasePage.tsx
@@ -1,9 +1,32 @@
 import React from "react";
 
+const databaseSchema = ["id", "type", " weight", "battery", "data_wiped", "bin"] //column names
+
+const dummyData = [
+  [1, "laptop", 10, true, true, 2],
+  [2, "phone", 1, true, false, 2],
+  [3, "mouse", 0.5, true, true, 2]
+]
+
 const DatabasePage = () => {
   return (
     <div>
-      <img src="src\assets\tablePlaceholder.png" alt="" />
+      {/* <img src="src\assets\tablePlaceholder.png" alt="" /> */}
+      <table>
+        <tr>
+          {databaseSchema.map((columnName)=>{
+            return <th>{columnName}</th>
+          })}
+        </tr>
+        {dummyData.map((row, rowIndex) => (
+          <tr>
+          {row.map((dataPoint, colIndex) => (
+            <td key={`${rowIndex}-${colIndex}`}>{dataPoint + ""}</td>
+          ))}
+          </tr>
+        ))}
+
+      </table>
     </div>
   );
 };
