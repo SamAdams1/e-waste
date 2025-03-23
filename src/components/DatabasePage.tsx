f9261d2eae5f1365cb37c0e1d5207b34c05b76c8
Sam Adams <sammyadams04@gmail.com>
2025-03-23 18:15:51 -0400

    type error and react console error fixes

--- src/components/DatabasePage.tsx
+++ src/components/DatabasePage.tsx
@@ -1,19 +1,12 @@
 import { useState, useEffect } from "react";
 import { IEwaste } from "../models/Ewaste";
-import { error } from "console";
-import { Interface } from "readline/promises";
 
 const databaseSchema = ["id", "type", " weight", "battery", "data_wiped", "bin"] //column names
 
-const dummyData = [
-  [1, "laptop", 10, true, true, 2],
-  [2, "phone", 1, true, false, 2],
-  [3, "mouse", 0.5, true, true, 2]
-]
 interface mongoResponse { ewaste: [IEwaste]}
 
 const DatabasePage = () => {
-  const [databaseData, setDatabaseData] = useState<IEwaste[]>([]);
+  const [databaseData, setDatabaseData] = useState<[IEwaste]>();
   
   const getDatabaseData = async (): Promise<[IEwaste] | undefined> => {
     try {
@@ -40,13 +33,13 @@ const DatabasePage = () => {
     fetchData();
   }, []);
 
-  return !databaseData ? (<>loading</>) : (
+  return !databaseData ? (<>loading...</>) : (
     <div>
       <table>
         <tbody>
         <tr>
-          {databaseSchema.map((columnName)=>(
-            <th>{columnName}</th>
+          {databaseSchema.map((columnName, index)=>(
+            <th key={columnName + index}>{columnName}</th>
           ))}
         </tr>
           {databaseData.map( (obj, index) => (
