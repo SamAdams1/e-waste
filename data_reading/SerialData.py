76f03b735cf77c0273c7009c47fc9b971a1439f1
Matt Eagan <mteagan2004@gmail.com>
2025-04-08 14:58:02 -0400

    Serial Read from Bin Complete
    Python script successfully reads Serial output from Arduino module with readings from Ultrasonic Sensors and outputs to .json format
    
    Does not include interface for weight. Will be added once load cells are completed and sketch has been written.
    
    Next Step: Separate Python Script will detect modifications of json files and will push information to MongoDB.

--- data_reading/SerialData.py
+++ data_reading/SerialData.py
@@ -1,16 +1,29 @@
 # ************************************************************
 #  Name: Matthew Eagan & Sam Adams & Sam Norton & Liem Tran
 #        & Brennan Khengadi & Chase Aidala
-#  Project: ScEnE - E6 E-Waste Bins
+#  Project: ScEnE - E-Bins by E6
 #  File Description: A Python script that reads data from an
 #                    Arduino module and sends it to MondoDB
-#  Last Modified: Mar. 27, 2024
+#  Last Modified: April 8, 2024
 # ************************************************************
 
 import serial.tools.list_ports as coms
-import serial
+import serial, re, os, json
 
 print("Welcome to the E6 Interface!\n")
+
+# User identifies bin being monitored by this script
+print("Enter the number of the bin you are monitoring below")
+binNo = input("Bin #: ")
+
+jsonName = "data_reading\\bin" + str(binNo) + ".json" 
+file = os.path.join(os.getcwd(), jsonName)
+# print(file)
+
+binInfo = {
+    "Distance": 0.0,
+    "Weight" : 0.0
+}
 
 # Initialize System Serial
 systemSerial = serial.Serial()
@@ -30,30 +43,72 @@ aPort = input("\nSelect the port through which you would like to read from your 
 uPort = aPort
 
 #Validate the user entered a correct COM port
-while (uPort == aPort):
-    for port in range(0, len(portlist)):
-        if (portlist[port].startswith("COM" + str(aPort))):
-            uPort = "COM" + str(aPort)
-            aPort = ""
-            print("You have selected: " + uPort)
-            break
-        elif (portlist[port].startswith(str(aPort))):
-            uPort = str(aPort)
-            aPort = ""
-            print("You have selected: " + uPort)
-            break
+while (uPort == aPort): # Iterate through this section until a valid port is selected
+    try:
+        for port in range(0, len(portlist)):
+            if (portlist[port].startswith("COM" + str(aPort))): # User identified port by COM number
+                uPort = "COM" + str(aPort) # Reformat user entry to full name
+                aPort = ""
+                print("You have selected: " + uPort)
+                break
+            elif (portlist[port].startswith(str(aPort))): # User identified port by full name
+                uPort = str(aPort)
+                aPort = ""
+                print("You have selected: " + uPort)
+                break
+
+        if (uPort == aPort): # User enters a nonexisting port
+            print("That is not a valid port!")
+            aPort = input("\nSelect the port through which you would like to read from your system: ")
+
+        if (uPort != aPort): # Attempts to open the port for serial communications if a valid input is entered
+            # Inititalize Serial Port functionality
+            systemSerial.port = uPort
+            systemSerial.open()
     
-    if (uPort == aPort):
-        print("That is not a valid port!")
+    except: # Prevents the script from crashing if the COM port is unavailable
+        print("This port is unavailable!")
         aPort = input("\nSelect the port through which you would like to read from your system: ")
+        uPort = aPort
+print("Successfully reading from: " + uPort)
 
-# Inititalize Serial Port functionality
-print(uPort)
-systemSerial.port = uPort
-systemSerial.open()
-print("Success!")
-
+# Continuously read from the Arduino
 while True:
     if systemSerial.in_waiting:
-        data = systemSerial.readline()
-        print(data.decode('utf').rstrip("\n"))
+        # # Read the data sent by the Arduino and decode result into strings
+        # data = systemSerial.readline() 
+        # data = data.decode('utf').rstrip("\n")
+
+        # # Extract numeric values sent from the Arduino and save as a list
+        # readings = re.findall(r'\d+', data)
+
+        readings = [0, 0, 0, 0]
+
+        for x in range(0,4):
+            # Read the data sent by the Arduino and decode result into strings
+            data = systemSerial.readline() 
+            data = data.decode('utf').rstrip("\n")
+
+            # Extract numeric values sent from the Arduino and save as a list
+            match = re.search(r'\d+', data)
+            if match:
+                readings[x] = float(match.group())
+
+        # Save the readings list as integer values
+        sensor1, sensor2, sensor3, sensor4 = readings
+
+        # Display results
+        print("Sensor 1: " + str(sensor1) + "cm")
+        print("Sensor 2: " + str(sensor2) + "cm")
+        print("Sensor 3: " + str(sensor3) + "cm")
+        print("Sensor 4: " + str(sensor4) + "cm")
+
+        # Average distance modules to make data more interpretable
+        avgDistance = ((sensor1 + sensor2 + sensor3 + sensor4)/4)
+
+        print("Distance: " + str(avgDistance) + "cm")
+
+        binInfo["Distance"] = avgDistance
+
+        with open(os.path.join(os.getcwd(), jsonName), 'w+') as file:
+            json.dump(binInfo, file)
